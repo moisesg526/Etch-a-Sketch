1251df38953b76ad921d6a5e45e3783be8f38398
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-03-26 23:28:01 -0400

    Added a for loop to create a column of divs and added border properties in the css file

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const container = document.querySelector(".container");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.style.border = "solid 1px #000000";
-content.textContent = "HELLO"
-
-container.appendChild(content);
+for(let i = 0; i < 16; i++) {
+    const content = document.createElement("div");
+    content.classList.add("content");
+    container.appendChild(content);
+}
