5fe4dc80a66936b0c2686b29894ce50c1b3eeea5
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-03-27 23:02:53 -0400

    Added 2 four loops to return row of squares next to each other

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const container = document.querySelector(".container");
+const content = document.createElement("div");
+content.classList.add("content");
+container.appendChild(content);
+const row = document.createElement("div");
+row.classList.add("oneRow");
+content.appendChild(row);
+const second = document.createElement("div");
+second.classList.add("second");
+content.appendChild(second);
 
-for(let i = 0; i < 16; i++) {
-    const content = document.createElement("div");
-    content.classList.add("content");
-    container.appendChild(content);
-}
+function square() {
+
+}  for (let i = 0; i < 5; i++) {
+    const divRow = document.createElement("div");
+    divRow.classList.add("row");
+    row.appendChild(divRow);
+  }
+  for (let i = 0; i < 5; i++) {
+    const divSecondRow = document.createElement("div");
+    divSecondRow.classList.add("secondRow");
+    second.appendChild(divSecondRow);
+  }
