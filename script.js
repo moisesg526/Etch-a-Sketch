e4e99ac41b32fe628e34b4d9cf741210deb067e6
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-04-15 23:56:55 -0400

    Wrapped the code in parenthesis for the function squares and added some css to make the squares in a grid

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector(".container");
 
-function square() {}
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
+function square() {
+  for (let i = 0; i < 16; i++) {
     const squares = document.createElement("div");
     squares.classList.add("squares");
     container.appendChild(squares);
+    for (let j = 0; j < 16; j++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      container.appendChild(row);
+    }
   }
-
 }
+square();
