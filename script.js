727beb1693a8b812fcac675dc88c01cb35b80649
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-04-30 23:57:39 -0400

    Change function to reset when new size is set

--- script.js
+++ script.js
@@ -1,60 +1,55 @@
 const container = document.querySelector(".container");
 const sketchInput = document.createElement("div");
 sketchInput.classList.add("sketchInput");
+sketchInput.appendChild(btn);
 container.appendChild(sketchInput);
 
-
-let blockNum = 0;
+const squares = document.createElement("div");
+squares.classList.add("squares");
+container.appendChild(squares);
 
 let btn = document.createElement("button");
 btn.textContent = "Change number of Squares";
-btn.addEventListener("click", (e) => {
-  let squareSize = prompt(
-    "Enter Number of squares per side. No less than 0 and nomore than 100!"
-  );
-  if (squareSize <= 0 || squareSize > 100) {
-    alert("Please enter a number between 1 and 100");
-  } else {
-    square(squareSize);
-  }
+btn.addEventListener("click", () => {
+  resetSquares();
 });
 
-sketchInput.appendChild(btn);
-
-
-function clearGrid() {
-  while (container.firstChild) {
-    container.firstChild.remove();
-  }
+function resetSquares() {
+  let number = prompt("Pick a size between 1 and 100");
+  container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+  square(number);
 }
 
 function square(size) {
-  const squares = document.createElement("div");
-  squares.classList.add("squares");
-  container.appendChild(squares);
-  for (let i = 0; i < size; i++) {
-    let widthSize = squares.offsetWidth / size;
-    const column = document.createElement("div");
-    column.classList.add("column");
-    for (let j = 0; j < size; j++) {
-      blockNum++;
-      const block = document.createElement("div");
-      block.classList.add("block", `number${blockNum}`);
-      block.style.height = `${widthSize}px`;
-      block.style.width = `${widthSize}px`;
-      column.appendChild(block);
-    }
-    squares.appendChild(column);
-  }
-  changeColor(blockNum);
-}
+  squares.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  squares.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-function changeColor(color) {
-  for (let i = 1; i <= color; i++) {
-    let colorChange = "." + "number" + i;
-    let boxhover = document.querySelector(colorChange);
-    boxhover.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = "white";
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "black";
+    container.appendChild(square);
+
+    square.addEventListener("mouseover", (e) => {
+      square.style.backgroundColor = "white";
     });
+
+    function clearGrid() {
+      clear.addEventListener("click", (e) => {
+        square.style.backgroundColor = "black";
+      });
+    }
+
+    clearGrid();
   }
 }
+square(16);
+// function changeColor(color) {
+//   for (let i = 1; i <= color; i++) {
+//     let colorChange = "." + "number" + i;
+//     let boxhover = document.querySelector(colorChange);
+//     boxhover.addEventListener("mouseover", (e) => {
+//       e.target.style.backgroundColor = "white";
+//     });
+//   }
+// }
